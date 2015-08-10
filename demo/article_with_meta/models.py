d6f024802deb21c307b0a9dea8a3489fee42e33e
MrLYC <imyikong@gmail.com>
2015-08-10 23:52:44 +0800

    add article_with_meta models

--- demo/article_with_meta/models.py
+++ demo/article_with_meta/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class ArticleMeta(models.Model):
+    title = models.CharField(
+        max_length=255, db_index=True,
+        null=False, blank=False, default=None,
+    )
+    authot = models.CharField(
+        max_length=255, null=False,
+        blank=False, default=None,
+    )
+    update_on = models.DateTimeField(
+        auto_now_add=True, auto_now=True,
+    )
+
+    @property
+    def details(self):
+        pass
+
+
+class ArticleDetails(models.Model):
+    meta = models.ForeignKey(ArticleMeta)
+    content = models.CharField(
+        max_length=65535,
+    )
+    type = models.CharField(
+        max_length=255, choices=(
+            ("md", "markdown"),
+            ("txt", "text"),
+            ("rtxt", "richtext"),
+        )
+    )
