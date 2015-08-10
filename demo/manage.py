7833daaf261669160b40eeb4e7e98b39136f5b64
MrLYC <imyikong@gmail.com>
2015-08-10 23:33:39 +0800

    add article_with_meta pattern

--- demo/manage.py
+++ demo/manage.py
@@ -4,6 +4,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo.settings")
+    os.environ.setdefault("PYTHONPATH", os.path.dirname(__file__))
 
     from django.core.management import execute_from_command_line
 
